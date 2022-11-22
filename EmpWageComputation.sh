9ded1826bf67f1a9bb39c66dc49d6a484d7a6a09
rajpal229 <rajpalrawat229@gmail.com>
2022-11-22 11:55:23 +0530

    Store the Day and the Daily Wage along with the Total Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,6 +1,14 @@
 echo "Welcome to the Employee Wage Computation Problem"
+
+isFullTime=1;
+isPartTime=2;
+totalWorkingHour=0;
+empRatePerHr=20;
+numWorkingDays=20;
+day=1;
+
 function calculateWorkingHour() {
-	case  $ramdomCheck in
+	case  $randomCheck in
 		$isFullTime)
 			empHrs=8
 			;;
@@ -15,15 +23,11 @@ esac
 
 }
 
-totalWorkingHour=0;
-empRatePerHr=20;
-numWorkingDays=20;
 
 while [[ $day -le 20 && totalWorkingHour -lt 40 ]]
 
 do
 randomCheck=$((RANDOM%3));
-
 empHrs=$(calculateWorkingHour $randomCheck);
 totalWorkingHour=$(($totalWorkingHour+$empHrs));
 
@@ -34,7 +38,12 @@ break;
 fi
 salary=$(($empHrs*$empRatePerHr));
 totalSalary=$(($totalSalary + $salary));
+echo "Day = "$day
+echo "Daily Wage = "$salary
+Dailywage[(($day))]=$(($salary));
 ((day++));
 done
 
 echo "Employee has earned $totalSalary in a month (Total working hour : $totalWorkingHour)";
+echo "DailyWage: "${Dailywage[@]}
+echo "Number of Days worked: "$(($day-1))
